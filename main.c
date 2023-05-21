81c9d1cf7e204510c19f2034e110b4798dde364e
Kasper Thörnsten <kasperthornsten@gmail.com>
2023-05-21 20:46:33 +0200

    Implement some more tests for dArray code

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include "Header Files/dArray.h"
 #include "Header Files/file.h"
 #include "Header Files/lexer.h"
 #include "Header Files/parser.h"
@@ -5,13 +6,21 @@
 int main(void) 
 {
 	FILE* sourceFile = openFile("../LanguageCode.cb", "r", (long int)NULL);
-
 	char* sourceCode = readAll(sourceFile);
 
-	printf(sourceCode);
+	//Token* tokens = lexify(sourceCode);
 
+	//free(tokens);
 	free(sourceCode);
 	fclose(sourceFile);
+
+	int32Array* test = initInt32Array(12);
+	for(int idx = 1; idx <= 100; idx++)
+	{
+		pushInt32(&test, idx);
+	}
+	printInt32Array(&test);
+	freeInt32Array(&test);
 
   	return 0;
 }
