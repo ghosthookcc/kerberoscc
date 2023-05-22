92f8e0c054a2ab80b78821d0856489450fc6b337
ghost <ghosthookcc@horatio.local>
2023-05-21 20:27:09 -0400

    Implement tests for new and old dynamic array types

--- main.c
+++ main.c
@@ -14,13 +14,40 @@ int main(void)
 	free(sourceCode);
 	fclose(sourceFile);
 
-	int32Array* test = initInt32Array(12);
-	for(int idx = 1; idx <= 100; idx++)
+	i32Array* test = initI32Array(12);
+	for (int idx = 1; idx <= 100; idx++)
 	{
-		pushInt32(&test, idx);
+		pushI32(&test, idx);
 	}
-	printInt32Array(&test);
-	freeInt32Array(&test);
+	printI32Array(&test);
+	freeI32Array(&test);
+
+	f32Array* test2 = initF32Array(12);
+	for (int idx = 1; idx <= 100; idx++)
+	{
+		pushF32(&test2, (float)(idx+0.5f));
+	}
+	printF32Array(&test2);
+	freeF32Array(&test2);
+
+	charArray* test3 = initCharArray(12);
+	for (int idx = 48; idx <= 122; idx++)
+	{
+		pushChar(&test3, (char)idx);
+	}
+	printCharArray(&test3);
+	freeCharArray(&test3);
+
+	stringArray* test4 = initStringArray(12);
+	charArray* string = NULL;
+	for (int idx = 1; idx <= 10; idx++)
+	{
+		string = initCharArray(10);
+		pushChar(&string, 'h');
+		pushString(&test4, string);
+	}
+	printStringArray(&test4);
+	freeStringArray(&test4);
 
   	return 0;
 }
